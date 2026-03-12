997dd07e55945298092e13a9c203f0ccd9584560
Joe Rebbeck <joerebbeck@hotmail.com>
2026-03-12 21:27:47 +0100

    Add language strings for assignment feedback element

---
+++ lang/en/customcertelement_assignfeedback.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of the customcert module for Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Language strings for customcertelement_assignfeedback.
+ *
+ * @package   customcertelement_assignfeedback
+ * @copyright 2026 Joe Rebbeck <tjr@the-ela.com>
+ * @license   https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['assignment']           = 'Assignment';
+$string['chooseassignment']     = 'Choose an assignment...';
+$string['feedbacknotavailable'] = 'Feedback not available';
+$string['pluginname']           = 'Assignment feedback';
+$string['privacy:metadata']     = 'The assignment feedback element does not store any personal data.';
